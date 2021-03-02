7743ffea3b226ffd1b2df85cf5681d2e522acadd
Shobhit Yadav <shobhit999000@gmail.com>
2021-03-02 23:48:13 +0530

    Completed

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import json
+
+import requests
+r = requests.get("http://newsapi.org/v2/top-headlines?country=in&apiKey=c4c10316899241c88721904ea2b93a3d")
+
+data =r.json()
+articles = data['articles']
+
+
 
 def speak(str):
     from win32com.client import Dispatch
     speak = Dispatch("SAPI.SpVoice")
     speak.Speak(str)
-
+count = 1
 if __name__ == '__main__':
-    speak("Hello everyone! how is it going?")
+    for items in articles:
+        speak(f"Todays News are !"+ f"News no {count} is" +  items['title'])
+        count = count + 1
+        print(items['title'])
